5a74a5d504c78ead56734447143c39f74cf5471d
Astrid Pettersen <astridpettersen13@gmail.com>
2023-03-22 06:57:36 +0100

    feat: add a description to the moves section

--- src/components/InfoContainer.tsx
+++ src/components/InfoContainer.tsx
@@ -9,35 +9,6 @@ const InfoContainer: React.FunctionComponent<Props> = ({ pokemon }) => {
   if (pokemon) {
     return (
       <div className='infoContainer'>
-
-
-
-        <div>
-          <div className='topText'>
-        <span>{pokemon.types[0].type.name} pokémon</span>
-        <span>{pokemon.name}</span>
-        </div>
-        <img className='Img'
-          src={pokemon.sprites.other.dream_world.front_default}
-          alt={`${pokemon.name} illustration`}
-        />
-        <div className='cardImg'>
-        <img src={require(`../cards/${pokemon.types[0].type.name}.png`)}
-        />
-        </div>
-        <div className='bottomText'>
-        <span>Id: {pokemon.id}</span>
-        <span>Height: {pokemon.height * 10} cm</span>
-        <span>Weight: {pokemon.weight / 10} kg</span>
-        </div>
-        <div className='moves'>
-        <span>Moves: {pokemon.moves[0].move.name}</span>
-        </div>
-      </div>
-
-
-
-
         <img
           src={pokemon.sprites.other.dream_world.front_default}
           alt={`${pokemon.name} illustration`}
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -10,19 +10,19 @@ const PokemonCard: React.FunctionComponent<Props> = ({ pokemon }) => {
     return (
       <div className='pokemonCard' style={{backgroundImage: `url(`+ require(`../cards/${pokemon.types[0].type.name}.png`) + `)`}}>
         <div className='title-row'>
-        <span>{pokemon.types[0].type.name} pokémon</span>
-        <h1>{pokemon.name}</h1>
+          <span>{pokemon.types[0].type.name} pokémon</span>
+          <h1>{pokemon.name}</h1>
         </div>
         <div className='pokemon-img'>
-        <img
-          src={pokemon.sprites.other.dream_world.front_default}
-          alt={`${pokemon.name} illustration`}
-        />
+          <img
+            src={pokemon.sprites.other.dream_world.front_default}
+            alt={`${pokemon.name} illustration`}
+          />
         </div>
         <div className='pokemon-info'>
-        <span className='info-span'>Id: {pokemon.id}</span>
-        <span className='info-span'>Height: {pokemon.height * 10} cm</span>
-        <span className='info-span'>Weight: {pokemon.weight / 10} kg</span>
+          <span className='info-span'>Id: {pokemon.id}</span>
+          <span className='info-span'>Height: {pokemon.height * 10} cm</span>
+          <span className='info-span'>Weight: {pokemon.weight / 10} kg</span>
         </div>
         <div className='pokemon-ability'>
           <span className='ability-span'>{pokemon.abilities[0].ability.name}</span>
@@ -30,11 +30,18 @@ const PokemonCard: React.FunctionComponent<Props> = ({ pokemon }) => {
         <h2>Moves:</h2>
         <div className='pokemon-moves'>
           <span className='moves-header'>{pokemon.moves[0].move.name}</span>
-          <span className='moves-description'>description</span>
+          <span className='moves-description'>
+            In this two-turn attack, blades of wind
+            hit opposing Pokémon on the second turn.
+            Critical hits land more easily.
+            </span>
         </div>
         <div className='pokemon-moves'>
           <span className='moves-header'>{pokemon.moves[1].move.name}</span>
-          <span className='moves-description'>description</span>
+          <span className='moves-description'>
+            A frenetic dance to uplift the fighting spirit. 
+            This sgarply raises the user's Attack stat.
+          </span>
         </div>
       </div>
     );
